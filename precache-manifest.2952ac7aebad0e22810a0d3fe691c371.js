77f698d787a6763e80a0cc2ca6c21d881184ea52
RDXKIRITO <trialasp9@gmail.com>
2020-06-16 12:35:26 +0530

    Updates

---
+++ precache-manifest.2952ac7aebad0e22810a0d3fe691c371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea007b24f2f625e8428af672beb0d0a",
+    "url": "/githubpagesReactapp/index.html"
+  },
+  {
+    "revision": "4caa186443d9b45e7a03",
+    "url": "/githubpagesReactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be9bbeee551b722f4bc2",
+    "url": "/githubpagesReactapp/static/js/2.c9bc4428.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubpagesReactapp/static/js/2.c9bc4428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4caa186443d9b45e7a03",
+    "url": "/githubpagesReactapp/static/js/main.11055a2a.chunk.js"
+  },
+  {
+    "revision": "35ea5317666f44852cab",
+    "url": "/githubpagesReactapp/static/js/runtime-main.c9149d60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpagesReactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
